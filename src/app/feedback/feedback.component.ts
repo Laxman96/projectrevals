a90a6f256d820c2df72b34609e20482d8f3dd1d9
Laxman V <laxman.scu@gmail.com>
2021-06-27 18:31:07 +0530

    feedback

---
+++ src/app/feedback/feedback.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-feedback',
+  templateUrl: './feedback.component.html',
+  styleUrls: ['./feedback.component.css']
+})
+export class FeedbackComponent implements OnInit {
+  feedbackForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, private route:Router) { }
+
+  ngOnInit() {
+    this.feedbackForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email, Validators.pattern("[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}")]],
+      phnumber: [null, [Validators.required, Validators.pattern("[0-9 ]{10}")]],
+      feedback:['',Validators.required]
+
+  }, {
+     
+  });
+  }
+
+  get f() { return this.feedbackForm.controls; }
+
+  onSubmit() {
+    // executes the conditions here if form is invalid
+    if (this.feedbackForm.valid) {
+      console.log("Form Submitted!"); 
+      let values = this.feedbackForm.value;  
+      this.feedbackForm.reset();
+    this.feedbackForm.setErrors(null); // could be removed
+    this.feedbackForm.updateValueAndValidity();
+      setTimeout(() => {
+        alert('SUCCESS!! :-)\n\n' + JSON.stringify(values, null, 4));   
+        this.route.navigate(['/list'], { queryParams: { sort: 'LowTohigh' } });
+        }, 100);
+
+    }
+      console.log(this.submitted,'hhgh');
+      // stop here if form is invalid
+      if (this.feedbackForm.invalid) {
+          return;
+      }  
+       
+  }
+
+  onReset() {
+      this.submitted = false;
+      this.feedbackForm.reset();
+      this.route.navigate(['/list'], { queryParams: { sort: 'LowTohigh' } });
+  }
+
+}
